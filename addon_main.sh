94ff99c66297fe32dbfea1ca17251dc8aa67f6bc
Sergey <polumish@gmail.com>
2025-04-19 23:15:05 +0300

    Update addon_main.sh
    remove venv

--- addon_main.sh
+++ addon_main.sh
@@ -4,7 +4,7 @@ MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
-/app/venv/bin/python3 main.py pair-only
+python3 main.py pair-only
 
 
 MQTT_HOST=$MQTT_HOST MQTT_USER=$MQTT_USER MQTT_PASSWORD=$MQTT_PASSWORD \
